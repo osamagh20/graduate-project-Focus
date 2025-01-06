c5070481c22a17d586169696e9484aee9b4c41ac
Hadeel-aldawsari <hadeel.amg99@gmail.com>
2025-01-06 14:33:19 +0300

    Delete src directory

--- src/main/java/com/example/focus/Repository/RentToolsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.focus.Repository;
-
-import com.example.focus.Model.RentTools;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RentToolsRepository extends JpaRepository<RentTools,Integer> {
-}
